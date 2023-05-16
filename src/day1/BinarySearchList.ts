52814072ac1a07e3e9c848f4baf24cb547983e44
Szymon Szadkowski <szymon.szadkowski@wolterskluwer.com>
2023-05-16 15:15:40 +0200

    Binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,22 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let lo = 0;
+    let hi = haystack.length;
 
+    let found = false;
+
+    do {
+        let middle = Math.floor(lo + (hi - lo) / 2);
+
+        let value = haystack[middle];
+
+        if (value === needle) {
+            found = true;
+        } else if (needle < value) {
+            hi = middle;
+        } else {
+            lo = middle + 1;
+        }
+    } while (!found && lo < hi);
+
+    return found;
 }
