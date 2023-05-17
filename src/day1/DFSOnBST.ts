f48bd4d28580ea35c29a2e9dab895664ef0b1f08
Szymon Szadkowski <szymon.szadkowski@wolterskluwer.com>
2023-05-17 08:49:09 +0200

    Depth First Search - Binary Search Tree

--- src/day1/DFSOnBST.ts
+++ src/day1/DFSOnBST.ts
@@ -1,3 +1,11 @@
-export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+function walk(node: BinaryNode<number> | null, value: number): boolean {
+    if (!node) return false;
 
+    if (node.value === value) return true;
+
+    return walk(node.left, value) || walk(node.right, value);
+}
+
+export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+    return walk(head, needle);
 }
