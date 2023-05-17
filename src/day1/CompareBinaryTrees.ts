96ab79acc8448ce761138a8ff043b9574c2e29d4
Szymon Szadkowski <szymon.szadkowski@wolterskluwer.com>
2023-05-17 09:06:27 +0200

    Compare Binary Trees

--- src/day1/CompareBinaryTrees.ts
+++ src/day1/CompareBinaryTrees.ts
@@ -1,3 +1,19 @@
-export default function compare(a: BinaryNode<number> | null, b: BinaryNode<number> | null): boolean {
+function walk(
+    a: BinaryNode<number> | null,
+    b: BinaryNode<number> | null,
+): boolean {
+    if (a === null && b === null) return true;
 
+    if (a === null || b === null) return false;
+
+    if (a.value !== b.value) return false;
+
+    return walk(a.left, b.left) && walk(a.right, b.right);
+}
+
+export default function compare(
+    a: BinaryNode<number> | null,
+    b: BinaryNode<number> | null,
+): boolean {
+    return walk(a, b);
 }
