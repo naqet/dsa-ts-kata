8693d5bd3f795fc6edb7a5949d58f47793a4ea9b
Szymon Szadkowski <szymon.szadkowski@wolterskluwer.com>
2023-05-17 08:49:22 +0200

    Breath First Search - Binary Tree

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,28 @@
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+    let queue = [head];
 
+    let found = false;
+
+    while (!found && queue.length) {
+        let curr = queue.shift();
+
+        if (!curr) {
+            break;
+        }
+
+        if (curr.value == needle) {
+            found = true;
+            break;
+        }
+
+        if (curr.left) {
+            queue.push(curr.left);
+        }
+
+        if (curr.right) {
+            queue.push(curr.right);
+        }
+    }
+
+    return found;
 }
